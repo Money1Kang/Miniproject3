90aedd736540b4569e3c029f22c7f4690154dc21
uyggnodkrap <uyggnodkrap26@gmail.com>
2022-05-30 22:23:50 +0900

    ⚒️ REFACT : Controller에서 Service로 분기
    controller가 일을하고 있길래 service에게 일 시켰습니다.

--- src/main/java/com/refrigerator/controller/RefrigeratorController.java
+++ src/main/java/com/refrigerator/controller/RefrigeratorController.java
@@ -13,30 +13,43 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.refrigerator.model.Menu;
+import com.refrigerator.service.RefrigeratorService;
 
 @RestController
 public class RefrigeratorController {
 	private List<Menu> menus;
-	
+	private RefrigeratorService refrigeratorService;
+
+
+
+	public RefrigeratorController(RefrigeratorService refrigeratorService) {
+		super();
+		this.refrigeratorService = refrigeratorService;
+	}
+
 	@RequestMapping("/Menu")
 	public List<Menu> allMenu(@RequestParam("choice") String choice) {
-		
+
 		// em.find로 데이터 전체 조회가 불가능해서 JPQL 사용
-		List<Menu> resultList=null;
+
 		if (choice.equals("allMenu")) {
-			
-			EntityManagerFactory emf = Persistence.createEntityManagerFactory("Miniproject3");
-			EntityManager em = emf.createEntityManager();
-			
-			// 모든 데이터 가져오는 쿼리문
-			String jpql = "SELECT m FROM Menu as m";
-			Query query = em.createQuery(jpql);
-			
-			// 쿼리 실행
-			resultList = query.getResultList();
-		}
-		// 객체 json으로 웹에 출력. URL주소 : http://localhost:8080/refrigerator/Menu?choice=allMenu => POSTMAN에서 이 URL 조회시 데이터 출력
-		return resultList; 
-		
+			return refrigeratorService.allMenu();
+		} 
+
+		else if (choice.equals("deleteMenu")){
+			return refrigeratorService.deleteMenu();
+		} 
+
+		else if (choice.equals("addMenu")) {
+			return refrigeratorService.addMenu();
+
+		} 
+
+		return refrigeratorService.modifyMenu();
 	}
+
+
+
+
+
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -7,17 +7,16 @@
 <title>레시피</title>
 </head>
 <body>
-
-	
 	<form method="get" action="Menu">
-		<label><input type="radio" name="choice" value="allMenu">주문 조회</label>
-
+	
+	  <label><input type="radio" name="choice" value="allMenu">주문 조회</label>
       <label><input type="radio" name="choice" value="addMenu">주문 추가</label>
       <label><input type="radio" name="choice" value="modifyMenu">주문 수정</label>
       <label><input type="radio" name="choice" value="deleteMenu">주문 삭제</label>
       
-
-      <p><input type="submit" value="Submit"> <input type="reset" value="Reset"></p>
+      <p>
+      	<input type="submit" value="Submit"> <input type="reset" value="Reset">
+      </p>
     </form>
 </body>
 </html>
