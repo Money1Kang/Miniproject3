d7e6c839ceab9db1df97aa9b90cf7b9d571cf212
MIHEE PARK <44748142+PMH2906@users.noreply.github.com>
2022-05-30 19:48:06 +0900

    ⭐ FEAT : allMenu

--- src/main/java/com/refrigerator/controller/RefrigeratorController.java
+++ src/main/java/com/refrigerator/controller/RefrigeratorController.java
@@ -1,9 +1,12 @@
 package com.refrigerator.controller;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,28 +16,27 @@ import com.refrigerator.model.Menu;
 
 @RestController
 public class RefrigeratorController {
+	private List<Menu> menus;
 	
-	@RequestMapping("/add")
-//	@RequestParam("menuName") String menuName)
-	public void ex4() {
+	@RequestMapping("/Menu")
+	public List<Menu> allMenu(@RequestParam("choice") String choice) {
 		
-//		// DB처리 및 엔터티 반환
-//		Refrigerator refrigerator = new Refrigerator();
-//		refrigerator.setMenuName(menuName);
-////		refrigerator.setId(1);
-////		refrigerator.setRecipe(null);
-////		refrigerator.setSource(null);
-////		refrigerator.setTime(null);
-//		return refrigerator;
+		// em.find로 데이터 전체 조회가 불가능해서 JPQL 사용
+		List<Menu> resultList=null;
+		if (choice.equals("allMenu")) {
+			
+			EntityManagerFactory emf = Persistence.createEntityManagerFactory("Miniproject3");
+			EntityManager em = emf.createEntityManager();
+			
+			// 모든 데이터 가져오는 쿼리문
+			String jpql = "SELECT m FROM Menu as m";
+			Query query = em.createQuery(jpql);
+			
+			// 쿼리 실행
+			resultList = query.getResultList();
+		}
+		// 객체 json으로 웹에 출력. URL주소 : http://localhost:8080/refrigerator/Menu?choice=allMenu => POSTMAN에서 이 URL 조회시 데이터 출력
+		return resultList; 
 		
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("Miniproject3");
-		EntityManager em = emf.createEntityManager();
-		EntityTransaction tx = em.getTransaction();
-		
-		tx.begin();
-		
-		Menu menu = em.find(Menu.class, 1);
-		System.out.println(menu);
-		tx.commit();
 	}
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -4,13 +4,20 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>레시피</title>
 </head>
 <body>
-	<form action="add">
-		<label for="name">메뉴이름</label><br>
-		<input id="menuName" type="text" name="menuName"><br> 
-		<input type="submit">        
-	</form>
+
+	
+	<form method="get" action="Menu">
+		<label><input type="radio" name="choice" value="allMenu">주문 조회</label>
+
+      <label><input type="radio" name="choice" value="addMenu">주문 추가</label>
+      <label><input type="radio" name="choice" value="modifyMenu">주문 수정</label>
+      <label><input type="radio" name="choice" value="deleteMenu">주문 삭제</label>
+      
+
+      <p><input type="submit" value="Submit"> <input type="reset" value="Reset"></p>
+    </form>
 </body>
 </html>
