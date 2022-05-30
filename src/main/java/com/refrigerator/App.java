3525a4e94c18d26a4190a6ab466e69abc46f4a99
uyggnodkrap <uyggnodkrap26@gmail.com>
2022-05-30 10:30:02 +0900

    ⭐ FEAT : create model

--- src/main/java/com/refrigerator/App.java
+++ src/main/java/com/refrigerator/App.java
@@ -1,9 +1,16 @@
 package com.refrigerator;
 
+import com.refrigerator.model.Refrigerator;
+
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println(123);
+		
+		Refrigerator r = Refrigerator.builder().id(1).build();
+		System.out.println(r);
+		
 
 	}
 
